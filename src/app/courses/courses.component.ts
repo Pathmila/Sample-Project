41373ff21bf6051e24ce2c9d7760ded9b2f7fc5e
Pathmila <asinipathmila@gmail.com>
2021-05-11 23:56:09 +0530

    component - courses

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  course = "Angular";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
